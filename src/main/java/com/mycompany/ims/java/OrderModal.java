ec00191b79a9edcbde50b331623b5a77bed37706
Jessie James Calvo <jcalvo_220000000014@uic.edu.ph>
2022-09-25 17:03:25 +0800

    Edit the column names to Product Name, Quantity, Price

--- src/main/java/com/mycompany/ims/java/OrderModal.java
+++ src/main/java/com/mycompany/ims/java/OrderModal.java
@@ -71,13 +71,13 @@ public class OrderModal extends javax.swing.JDialog {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Product Name", "Quantity", "Price"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
